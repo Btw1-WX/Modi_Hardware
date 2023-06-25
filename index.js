06e5ae81eac87a2ff8f33e25b90c4b426c55ca4b
BTW - 1 <btw1@wictronix.com>
2023-06-25 23:46:53 +0530

    Final Website

---
+++ index.js
@@ -0,0 +1,24 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    number: document.getElementById("number").value,
+  };
+
+  const serviceID = "service_odkp6jv";
+  const templateID = "template_uxbofsi";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("number").value="";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
