bde12452d9fa41dab2382ab00c2d90002378f672
BTW - 1 <btw1@wictronix.com>
2023-07-07 21:45:03 +0530

    update

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ function sendMail() {
   const serviceID = "service_odkp6jv";
   const templateID = "template_uxbofsi";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        document.getElementById("number").value="";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
+ 
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("number").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
     })
-    .catch(err=>console.log(err));
-
+    .catch(err => console.log(err));
 }
