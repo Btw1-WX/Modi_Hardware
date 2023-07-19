11a0d8b8d36f4b3256288b5382e08dba0adeacd8
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-07-19 17:03:49 +0530

    products completed

--- js/theme.js
+++ js/theme.js
@@ -50,6 +50,26 @@
             }
         }
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:30,
+        items:2,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            1200:{
+                items: 1
+            }
+        }
+    })
     
     function projectMasonry(){
         if ( $('#projects').length ){
